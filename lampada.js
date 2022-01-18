4d56bf29aa4fa52028fcd598d1b094c5c6ae62a9
{ Walyson } <walysonmoura222@gmail.com>
2022-01-17 22:53:00 -0300

    Add files via upload

---
+++ lampada.js
@@ -0,0 +1,23 @@
+
+function clique(){
+				let body = document.querySelector("body")
+  let imagem = document.getElementById('imagem').src;
+  let lampadaDesligada = "https://i.ibb.co/6bz3GN1/bulb-off.png";
+  let lampadaLigada = "https://i.ibb.co/sWPkDc0/bulb-on.png";
+  let audio = new Audio('Audio/audio.mp3');
+  
+  audio.play()
+  body.classList.toggle('body')
+  window.navigator.vibrate(200);
+
+  if(imagem === lampadaLigada){
+    document.getElementById('imagem').src = lampadaDesligada;
+    
+  } else {
+   document.getElementById('imagem').src = lampadaLigada;
+  }
+  
+}
+
+
+ 
